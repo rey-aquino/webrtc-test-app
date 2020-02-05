417d75bcb434d7b4c3f2f42ece339f336a623127
Rey V. Aquino <49036511+includeiostreamsharp@users.noreply.github.com>
2020-02-05 19:26:52 +0800

    socket-io-client
    npm install socket-io-client --save

--- server.js
+++ server.js
@@ -9,9 +9,21 @@ const app = express()
 const port = 8080
 
 // app.get('/', (req, res) => res.send('Hello World'))
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(__dirname + '/.meteor/local/build'))
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/build/index.html')
 })
 
 const server = app.listen(port, () => console.log('Example App Listening to port: 8080'))
+
+io.listen(server)
+
+// https://www.tutorialspoint.com/socket.io/socket.io_quick_guide.htm
+const peers = io.of('/webrtcPeer')
+
+//keep a reference of all socket connections
+let connectedPeers = new Map()
+
+peers.on('connection', socket => {
+    console.log(socket.id)
+})
